4684bcf768ffe63aa8bb0e23ad78da6469c6091e
JyotiGhising1 <83853614+JyotiGhising1@users.noreply.github.com>
2024-05-02 09:13:39 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp)=> resp.json())
+  .then((data)=>{
+    renderBooks(data);
+    return data;
+  });
 }
 
 function renderBooks(books) {
@@ -14,4 +20,5 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  
 });
